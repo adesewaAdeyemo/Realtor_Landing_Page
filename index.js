f0ecfb80350023fe2e24f9aef99f92e6aa516b49
adesewaAdeyemo <adeyemoadesewa54@gmail.com>
2023-08-16 12:51:34 +0000

    added the auth page

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ faqItems.forEach((item) => {
 });
 
 
+
+
 const countdownElement = document.getElementById("countdown");
 const a = document.querySelector(".a");
 
@@ -28,7 +30,62 @@ countdownElement.textContent = timeText;
 
 if (seconds === 0) {
     clearInterval(countdownInterval);
-    //   countdownElement.textContent = 'Time\'s up!';
     countdownElement.style.color = "#000";
 }
 }, 1000);
+
+
+
+
+
+const monthlyBtn = document.getElementById("monthlyBtn");
+const annualBtn = document.getElementById("annualBtn");
+
+monthlyBtn.addEventListener("click", () => {
+  monthlyBtn.classList.add("activeBtn");
+  annualBtn.classList.remove("activeBtn");
+});
+
+annualBtn.addEventListener("click", () => {
+  annualBtn.classList.add("activeBtn");
+  monthlyBtn.classList.remove("activeBtn");
+});
+
+
+
+
+
+annualBtn.addEventListener("click", () => {
+  annualBtn.classList.add("activeBtn");
+  monthlyBtn.classList.remove("activeBtn");
+});
+function showForm(formId) {
+  const forms = document.querySelectorAll(".change-form");
+
+  forms.forEach((form) => {
+    form.style.display = "none";
+  });
+
+  const selectedForm = document.getElementById(formId);
+  if (selectedForm) {
+    selectedForm.style.display = "block";
+  }
+}
+
+
+
+
+function copyToClipboard(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+      const text = element.textContent;
+
+      navigator.clipboard.writeText(text)
+        .then(() => {
+          console.log('Text copied to clipboard');
+        })
+        .catch((error) => {
+          console.error('Error copying text to clipboard:', error);
+        });
+    }
+  }
